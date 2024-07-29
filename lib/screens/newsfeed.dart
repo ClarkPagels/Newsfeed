3a4c71e625079e87ac77cd7ac19a060dd3724bb5
Clark Pagels <Cjpagels2@gmail.com>
2024-07-29 14:38:08 -0400

    create newsfeed screen

---
+++ lib/screens/newsfeed.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/post_provider.dart';
+import './add_post_screen.dart';
+import '../widgets/post_item.dart';
+
+class NewsfeedScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Newsfeed'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => AddPostScreen(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Consumer<PostProvider>(
+        builder: (ctx, postProvider, _) => ListView.builder(
+          itemCount: postProvider.posts.length,
+          itemBuilder: (ctx, index) => PostItem(postProvider.posts[index]),
+        ),
+      ),
+    );
+  }
+}
